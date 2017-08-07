af91c6be6bbe5df3530602a31d004fafe0081f46
James Pogran <james.pogran@outlook.com>
2017-08-07 10:44:02 -0400

    update pre install script

--- ch06/01_pre_install_tasks.ps1
+++ ch06/01_pre_install_tasks.ps1
@@ -1,36 +1,22 @@
+############################################################################################
+##
+## From Learning PowerShell DSC (Packt)
+## by James Pogran (https://www.packtpub.com/networking-and-servers/learning-powershell-dsc)
+##
+############################################################################################
 
 ## this is only needed to compile the MOF on the pullserver, our chosen authoring station
-Install-Package -Name 'xWebAdministration' -RequiredVersion 1.7.0.0 -Force -ForceBootStrap
-Install-Package -Name 'xPSDesiredStateConfiguration' -RequiredVersion 3.4.0.0 -Force -ForceBootStrap
-
-## this is only needed to compile the MOF on the pullserver, our chosen authoring station
-Write-Warning "Copy 'xWebAdministration' Version 1.7.0.0 to c:\modules and to `$env:ProgramFiles\WindowsPowerShell\Modules"
-Write-Warning "Copy 'xPSDesiredStateConfiguration' Version 3.4.0.0 c:\modules and to `$env:ProgramFiles\WindowsPowerShell\Modules"
-
-## this is only needed if you don't already have your DSC Resources packaged
-. c:\vagrant\book\ch06\dsctooling.ps1
-if(-not(Test-Path 'c:\modules')){
-  mkdir 'c:\modules'
-  Write-Error "You must download $([string]::join(' and', $modules)) to your working directory to package them up in this step here"
-}
 $modules = @('xWebAdministration','xPSDesiredStateConfiguration')
-$modules | % {
-  New-DscResourceArchive -ModulePath (Join-Path 'C:\vagrant' $_) -Destinationpath (Join-Path 'c:\modules' $_) -Force
-}
-New-DscChecksum -Path 'c:\modules'
-
-## if you are using test machines that cannot resolve each other's hostnames
-if(-not (Select-String -Path 'C:\Windows\System32\drivers\etc\hosts' -Pattern '192.168.0.12')){
-  Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "`n192.168.0.12`tdsc-box1"
-  Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "`n192.168.0.13`tdsc-box2"
+$modules | ForEach-Object {
+  Install-Package -Name $_ -Force -ForceBootStrap
 }
 
 ## This is only required if you are using test machines without a AD domain cert or SSL cert that
 ## is on all machines
 $CertPath = 'C:\vagrant\servercert.cer'
-$cert = Get-ChildItem -Path Cert:\LocalMachine\My\ | ? { $_.Subject -eq 'CN=dsc-box1' }
+$cert = Get-ChildItem -Path Cert:\LocalMachine\My\ | Where-Object { $_.Subject -eq 'CN=dsc-box1' }
 if($cert.Count -eq 0){
-  $cert =  New-SelfSignedCertificate -DnsName 'dsc-box1' -CertStoreLocation cert:\LocalMachine\My
+  $cert =  New-SelfSignedCertificate -DnsName 'dsc-box1' -CertStoreLocation 'cert:\LocalMachine\My'
 }
 Export-Certificate -Cert "Cert:\LocalMachine\My\$($cert.ThumbPrint)" -FilePath $CertPath
 Import-Certificate -FilePath $CertPath -CertStoreLocation Cert:\LocalMachine\Root
@@ -44,12 +30,17 @@ Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value * -Force
 Set-NetFirewallProfile -All -Enabled false
 
 ## if you are using test machines that cannot resolve each other's hostnames
+if (-not (Select-String -Path 'C:\Windows\System32\drivers\etc\hosts' -Pattern '192.168.50.2')) {
+  Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "`n192.168.50.2`tdsc-box1"
+  Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "`n1192.168.50.4`tdsc-box2"
+}
+
 Invoke-Command -ComputerName 'dsc-box2' -Scriptblock {
   Set-NetFirewallProfile -All -Enabled false
   Set-Item -Path WSMan:\localhost\Client\TrustedHosts -Value * -Force
   Import-Certificate -FilePath "C:\vagrant\servercert.cer" -CertStoreLocation Cert:\LocalMachine\Root
-  if(-not (Select-String -Path "C:\Windows\System32\drivers\etc\hosts" -Pattern "192.168.0.12")){
+  if(-not (Select-String -Path "C:\Windows\System32\drivers\etc\hosts" -Pattern "192.168.50.2")){
     Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "`n192.168.0.12`tdsc-box1"
-    Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "`n192.168.0.13`tdsc-box2"
+    Add-Content -Path C:\Windows\System32\drivers\etc\hosts -Value "`n1192.168.50.4`tdsc-box2"
   }
 }
